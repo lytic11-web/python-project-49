6e507af7afca898577a39d1a989a312894951853
lytic11 <lytic11@gmail.com>
2025-11-18 18:25:56 +0300

    corrected even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,11 +1,13 @@
 import random
 
 
-def is_even(namber: int) -> bool:
-    return namber % 2 == 0
+def is_even(number: int) -> bool:
+    return number % 2 == 0
 
 
-def generate_round() -> tuple:
+def generate_round() -> tuple[str, str]:
+    """Generate a round for the even game."""
+    # For game purposes, using random is safe here
     number = random.randint(1, 100)
     question = str(number)
     correct_answer = 'yes' if is_even(number) else 'no'
